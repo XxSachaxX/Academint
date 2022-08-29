34d3cbcb5f9ed9f820dac4f6ae8cb732bf003d82
Sacha Gotainer <sgotainer@hotmail.com>
2022-08-29 18:01:13 +0200

    Pundint setup

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,13 +1,39 @@
 class CoursesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show]
   def index
+    @courses = policy_scope(:courses)
   end
 
   def show
+    authorize @course
   end
 
   def new
+    @course = Course.new
+    authorize @course
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.user = current_user
+    authorize @course
   end
 
   def edit
+    authorize @course
+  end
+
+  def update
+    authorize @course
+  end
+
+  def destroy
+    authorize @course
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :description, :level)
   end
 end
