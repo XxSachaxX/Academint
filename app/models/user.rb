195c205a49111df130c1f55a9f2503f1fa4460d7
Sebastien VILLEBRUN <sebastien.villebrun@gmail.com>
2022-08-30 18:12:10 +0200

    username to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
 end
