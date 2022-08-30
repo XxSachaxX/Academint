a805b931077176d77692c54bc5febeccd239d884
Sebastien VILLEBRUN <sebastien.villebrun@gmail.com>
2022-08-30 15:13:18 +0200

    Destroy User destroy all linked items

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,7 @@
 class Lecture < ApplicationRecord
   belongs_to :user
   belongs_to :lesson
-  has_one :chapter, through: :lesson
-  has_one :course, through: :chapter
+  has_one :chapter, through: :lesson, dependent: :destroy
+  has_one :course, through: :chapter, dependent: :destroy
   enum status: { inactive: 0, démarrée: 1, terminée: 2 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :lectures
-  has_many :designed_courses, class_name: 'Course'
-  has_many :classrooms
-  has_many :courses, through: :classrooms
-  has_many :chapters, through: :lessons
-  has_many :lessons, through: :lectures
-  has_many :designed_chapters, through: :designed_courses, source: :chapters
-  has_many :designed_lessons, through: :designed_chapters, source: :lessons
-  has_one_attached :photo
+  has_many :lectures, dependent: :destroy
+  has_many :designed_courses, class_name: 'Course', dependent: :destroy
+  has_many :classrooms, dependent: :destroy
+  has_many :courses, through: :classrooms, dependent: :destroy
+  has_many :chapters, through: :lessons, dependent: :destroy
+  has_many :lessons, through: :lectures, dependent: :destroy
+  has_many :designed_chapters, through: :designed_courses, source: :chapters, dependent: :destroy
+  has_many :designed_lessons, through: :designed_chapters, source: :lessons, dependent: :destroy
+  has_one_attached :photo, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
