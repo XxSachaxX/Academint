24b55724d9320408482f659b4dd7046b15b7106a
Sacha Gotainer <sgotainer@hotmail.com>
2022-08-30 15:39:43 +0200

    Seed ready and corrected dependents destroys on models

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :chapter
   has_many :lectures, dependent: :destroy
-  has_one :course, through: :chapter, dependent: :destroy
+  has_one :course, through: :chapter
   validates :content, presence: true
   has_many_attached :photos, dependent: :destroy
 end
